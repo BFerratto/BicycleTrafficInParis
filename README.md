4be7bb87bbf808c2207ff6519773a76c3f76e78b
Bruna <Bruna@Mac.fritz.box>
2025-03-30 21:38:35 +0200

    add dataset and structure app

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # BicycleTrafficInParis
 
+Data set: "comptage_velo_donnees_compteurs.csv"
+Source: opendate.paris.fr
+
+# Objectives 
+The main goal of this analysis is to understand bicycle traffic patterns in Paris and extract
+valuable insights that can support urban mobility planning, policy-making, and infrastructure
+development in the period from October 2023 to September 2024.
--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,30 @@
 import streamlit as st
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
 
-st.title('Bikes in Paris')
+df=pd.read_csv('bikes-paris.csv', sep=';')
 
-st.write('Introduction')
+st.title('Bicycle Traffic in Paris')
+st.sidebar.title('Table of contents')
+pages=['Introduction','Exploration', 'Data Visualization', 'Modelling']
+page=st.sidebar.radio('Go to', pages)
+
+if page == pages[0] : 
+  st.write("### Presentation of data")
+  st.dataframe(df.head())
+  st.write(df.shape)
+  st.dataframe(df.describe())
+
+  if st.checkbox('Show NA'):
+        st.dataframe(df.isna().sum())
+
+if page == pages[1]:
+    st.write('### DataVizualization')
+
+   
+if page == pages[2] :
+  st.write("### Modelling")
+
+ 
